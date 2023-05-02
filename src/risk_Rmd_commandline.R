23c58b232d8bf47055c6a8574a4047d105bdaba5
Erich Seamon <erichs@uidaho.edu>
2023-05-02 15:54:56 -0700

    modified supplemental - poisson offset

--- src/risk_Rmd_commandline.R
+++ src/risk_Rmd_commandline.R
@@ -1,3 +1,3 @@
 Sys.setenv(RSTUDIO_PANDOC="/opt/rstudio-server/bin/pandoc")
-rmarkdown::render("/mnt/ceph/erichs/git/IMCI-covid-riskfinal/Rmd/Risk_SupplementalA_2_trillian.Rmd")
+rmarkdown::render("/mnt/ceph/erichs/git/IMCI-covid-riskfinal/Rmd/Risk_SupplementalA_revised.Rmd")
 #nohup R CMD BATCH /mnt/ceph/erichs/git/IMCI-covid-riskfinal/src/risk_Rmd_commandline.R &
