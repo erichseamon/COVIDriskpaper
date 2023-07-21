888e2623b4d4f361ef55b38811ca6fd7f0c0117e
Erich Seamon <erichs@uidaho.edu>
2023-07-21 09:58:28 -0700

    remove /src code

--- src/risk_Rmd_commandline.R
+++
@@ -1,3 +0,0 @@
-Sys.setenv(RSTUDIO_PANDOC="/opt/rstudio-server/bin/pandoc")
-rmarkdown::render("/mnt/ceph/erichs/git/COVIDriskpaper/Risk_SupplementalA.Rmd")
-#nohup R CMD BATCH /mnt/ceph/erichs/git/COVIDriskpaper/src/risk_Rmd_commandline.R &
