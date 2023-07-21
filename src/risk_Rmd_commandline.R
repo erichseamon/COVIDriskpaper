c14af1bdff1615b08d2209661273d63bcf0adf13
Erich Seamon <erichs@uidaho.edu>
2023-07-21 09:41:57 -0700

    finalize manuscript supplemental

--- src/risk_Rmd_commandline.R
+++ src/risk_Rmd_commandline.R
@@ -1,3 +1,3 @@
 Sys.setenv(RSTUDIO_PANDOC="/opt/rstudio-server/bin/pandoc")
-rmarkdown::render("/mnt/ceph/erichs/git/IMCI-covid-riskfinal/Rmd/Risk_SupplementalA_rev4.Rmd")
-#nohup R CMD BATCH /mnt/ceph/erichs/git/IMCI-covid-riskfinal/src/risk_Rmd_commandline.R &
+rmarkdown::render("/mnt/ceph/erichs/git/COVIDriskpaper/Risk_SupplementalA.Rmd")
+#nohup R CMD BATCH /mnt/ceph/erichs/git/COVIDriskpaper/src/risk_Rmd_commandline.R &
